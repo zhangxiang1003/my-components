4a616436b70e97c415eb26065dcda02eb6e78321
zhangxiang2 <zhangxiang2@tuhu.cn>
2022-03-30 14:31:31 +0800

    feat: autoLoading Button开发

---
+++ src/components/MyButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-button v-bind="$attrs" :loading="loadingStatus" @click="handleClick">
+    <slot></slot>
+  </el-button>
+</template>
+
+<script>
+export default {
+  name: 'MyButton',
+  props: {
+    autoLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      loadingStatus: false
+    }
+  },
+  methods: {
+    handleClick() {
+      if (this.autoLoading) {
+        this.loadingStatus = true
+      }
+      this.$emit('click', () => {
+        this.loadingStatus = false
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <my-button type="primary" autoLoading @click="onClick">按钮</my-button>
+  </div>
+</template>
+<script>
+import MyButton from '@/components/MyButton.vue'
+export default {
+  name: 'Button',
+  components: {
+    'my-button': MyButton
+  },
+  methods: {
+    onClick(done) {
+      setTimeout(() => {
+        done()
+      }, 1000)
+    }
+  }
+}
+</script>
